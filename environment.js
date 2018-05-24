b452427e7ecd42d251618a2c9e9056c68198cbe7
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-05-23 22:38:06 -0400

    Export the env functions

--- environment.js
+++ environment.js
@@ -1,2 +1,7 @@
-const f = (func, ...args) => func(args);
-const sum = (...args) => args.reduce((acc, i) => acc += i,0);
+const f = (func, ...args) => func(...args);
+const sum = (...args) => args.reduce((acc, i) => acc += i, 0);
+
+module.exports = {
+  f,
+  sum,
+};
