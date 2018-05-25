b220c8f1bb086bf2b3a6135dbcdfa79bd14d2832
Vadim Brodsky <vadim.brodsky@gmail.com>
2018-05-25 15:17:33 -0400

    Use names from text

--- env/index.js
+++ env/index.js
@@ -1,6 +1,6 @@
 const env = {
-  f: (func, ...args) => func(...args),
-  sum: (...args) => args.reduce((acc, i) => (acc += i), 0),
+  f: (operator, ...operands) => operator(...operands),
+  sum: (...operands) => operands.reduce((acc, i) => (acc += i), 0),
 };
 
 module.exports = env;
